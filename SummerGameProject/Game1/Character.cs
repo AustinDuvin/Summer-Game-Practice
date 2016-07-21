4f508909b5894a0b9ca54bce912afc6e6756c5d0
unknown <axd6329@g.rit.edu>
2016-07-21 15:34:13 -0400

    Starting.

---
+++ SummerGameProject/Game1/Character.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Game1
+{
+    class Character
+    {
+        private String name;
+        private int maxHealth;
+        private int currentHealth;
+        private int damage;
+
+        public Character(String name, int maxHealth, int damage)
+        {
+            this.name = name;
+            this.maxHealth = maxHealth;
+            this.damage = damage;
+        }
+    }
+}
