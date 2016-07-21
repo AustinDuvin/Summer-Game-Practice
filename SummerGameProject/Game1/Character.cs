3e071322f2093a249773906f71d91b92b541f3fa
unknown <axd6329@g.rit.edu>
2016-07-21 15:58:06 -0400

    Started programming a character.

--- SummerGameProject/Game1/Character.cs
+++ SummerGameProject/Game1/Character.cs
@@ -5,18 +5,33 @@ using System.Text;
 
 namespace Game1
 {
-    class Character
+    public abstract class Character
     {
-        private String name;
+        /*private String name;
         private int maxHealth;
         private int currentHealth;
-        private int damage;
+        private int damage;*/
 
-        public Character(String name, int maxHealth, int damage)
+        public String Name { get; set; }
+
+        public int MaxHealth { get; set; }
+
+        public int CurrentHealth { get; set; }
+
+        public int Damage { get; set; }
+
+        /*public Character(String name, int maxHealth, int damage)
         {
             this.name = name;
             this.maxHealth = maxHealth;
             this.damage = damage;
+        }*/
+
+        public void DealDamage(Character target)
+        {
+            if (target.CurrentHealth - Damage >= 0) { target.CurrentHealth -= Damage; }
+
+            else { target.CurrentHealth = 0; }
         }
     }
 }
---
+++ SummerGameProject/Game1/Soldier.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Game1
+{
+    class Soldier : Character
+    {
+        public Soldier()
+        {
+            Name = "Soldier";
+            MaxHealth = 100;
+            CurrentHealth = MaxHealth;
+            Damage = 20;
+        }
+    }
+}
